ffd4d1e126a1ed28826753c666138024a6706677
Victor <koehlervictor@cc.ci.ufpb.br>
2020-12-09 10:45:33 -0300

    Refactor 1

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ class Controller(Parameters):
 	
 	def __init__(self):
 		# Preprocessing pipeline
-		self.data = self.prepare_data(Parameters.num_words, Parameters.seq_len)
+		self.pr = Preprocessing(Parameters.num_words, Parameters.seq_len)
+		self.data = self.prepare_data()
 		
 		# Initialize the model
 		self.model = TextClassifier(Parameters)
@@ -16,24 +17,26 @@ class Controller(Parameters):
 		# Training - Evaluation pipeline
 		Run().train(self.model, self.data, Parameters)
 		
-		
-	@staticmethod
-	def prepare_data(num_words, seq_len, source=None):
+
+	def prepare_data(self, source=None, split=True):
 		# Preprocessing pipeline
-		pr = Preprocessing(num_words, seq_len)
+		pr = self.pr
 		pr.load_data(source=source)
 		pr.clean_text()
 		pr.text_tokenization()
 		pr.build_vocabulary()
 		pr.word_to_idx()
 		pr.padding_sentences()
-		pr.split_data(dontsplit=False if source is None else True)
+		if split:
+			pr.split_data()
+		else:
+			pr.commit_data()
 
 		return {'x_train': pr.x_train, 'y_train': pr.y_train, 'x_test': pr.x_test, 'y_test': pr.y_test}
 	
 	def execute(self, inputdata : list):
-		pdata = self.prepare_data(Parameters.num_words, Parameters.seq_len, source=['']+inputdata)
+		pdata = self.prepare_data(source=['']+inputdata, split=False)
 		return Run().execute(self.model, pdata)[1:]
-		
+
 if __name__ == '__main__':
 	controller = Controller()
